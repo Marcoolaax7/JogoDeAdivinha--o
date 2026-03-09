023cd4f1c6b76aa4bdbef9feb934c131d88d8000
Marcoolaax7 <marcooliver2@yahoo.com>
2026-03-09 15:08:43 -0300

    Adicionado projeto inicial

---
+++ JogoDeAdvinhaçao.ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Data;
+using System.Diagnostics;
+
+Console.Clear();
+
+Console.WriteLine("--------------------------!");
+Console.WriteLine("Jogo de Advinhação!");
+Console.WriteLine("--------------------------!");
+
+Console.ReadLine();
+
+
+
