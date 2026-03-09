8b9b4e99d4b8a777494866805d12aa24ebdf3a72
Marcoolaax7 <marcooliver2@yahoo.com>
2026-03-09 15:19:17 -0300

    V1

--- JogoDeAdvinhaçao.ConsoleApp/Program.cs
+++ JogoDeAdvinhaçao.ConsoleApp/Program.cs
@@ -1,13 +1,25 @@
-﻿using System.Data;
-using System.Diagnostics;
+﻿
+
+// Objetivos / Passo-a-passo
+// 1. Nosso jogo deve aceitar o input do jogador e exibir o valor digitado.
+// 2. Nosso jogo deve gerar um numero secreto aleatório.
+// 3. Nosso jogo deve validar a tentativa do jogador e exibir uma mensagem.
+
+
 
 Console.Clear();
 
-Console.WriteLine("--------------------------!");
+Console.WriteLine("---------------------------");
 Console.WriteLine("Jogo de Advinhação!");
-Console.WriteLine("--------------------------!");
+Console.WriteLine("---------------------------");
+
+Console.Write("Digite um número entre 1 e 20; ");
+string? chute = Console.ReadLine();
+
+Console.WriteLine("O valor digitado foi : " + chute);
 
 Console.ReadLine();
+
 
 
 
