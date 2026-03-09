b58ae23671cffdebac1a2f0984e3e51471ad2ff2
Marcoolaax7 <marcooliver2@yahoo.com>
2026-03-09 16:01:10 -0300

    Adicionado validacao

--- JogoDeAdvinhaçao.ConsoleApp/Program.cs
+++ JogoDeAdvinhaçao.ConsoleApp/Program.cs
@@ -5,21 +5,54 @@
 // 2. Nosso jogo deve gerar um numero secreto aleatório.
 // 3. Nosso jogo deve validar a tentativa do jogador e exibir uma mensagem.
 
+using System.Security.Cryptography;
 
 
-Console.Clear();
+
+while (true == true)
+{
+    Console.Clear();
 
 Console.WriteLine("---------------------------");
 Console.WriteLine("Jogo de Advinhação!");
 Console.WriteLine("---------------------------");
 
-Console.Write("Digite um número entre 1 e 20; ");
-string? chute = Console.ReadLine();
+int numeroAleatorio = RandomNumberGenerator.GetInt32(1,21);
 
-Console.WriteLine("O valor digitado foi : " + chute);
+Console.Write("Digite um número entre 1 e 20: ");
+int numeroEscolhido = Convert.ToInt32(Console.ReadLine());
 
-Console.ReadLine();
 
+if (numeroAleatorio == numeroEscolhido)
+{
+    Console.WriteLine("---------------------------");
+    Console.WriteLine("Parabens voce acertou!");
+    Console.WriteLine("---------------------------");
+    
+}
+else if (numeroAleatorio > numeroEscolhido)
+{
+    Console.WriteLine("---------------------------");
+    Console.WriteLine("O número digitado é maior que o número secreto!.");
+    Console.WriteLine("---------------------------");
+}
+
+else 
+{
+    Console.WriteLine("---------------------------");
+    Console.WriteLine("O número digitado é menor que o número secreto!.");
+    Console.WriteLine("---------------------------");
+}
+Console.WriteLine("Deseja continuar? (s/n): ");
+string? opcaoContinuar = Console.ReadLine();
+
+if (opcaoContinuar.ToUpper() != "s" )
+    {
+        break;
+    }
+  Console.ReadLine();
+
+}
 
 
 
